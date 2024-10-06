99dd82b99eddc33c4f6970194669d8f71d38c91b
turotsdante <forstudypurposes23yiea@gmail.com>
2024-10-07 05:36:10 +1100

    Added passwords

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log("Hello! We're working hard (we being me) so stand by!");
 
+let passwordRead1 = prompt('Who gave me a butterfly hairclip? (first name lowercase): ');
+if (passwordRead1 !== "ann") { close() };
+let passwordRead2 = prompt('Who do I think is the cuddliest most lovely person in the world? (first name lowercase): ');
+if (passwordRead2 !== "jen") { close() };
+let passwordRead3 = prompt('Who is the person I need to show my love to most right now? (first name lowercase): ')
+if (passwordRead3 !== "marcus") { close() }
